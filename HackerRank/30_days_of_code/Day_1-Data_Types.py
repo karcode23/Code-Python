9e0bfa22174b1957dea058c6209985cd8eb550f0
Karthik.Shekar <karthik.shekar@yahoo.com>
2024-09-11 23:10:12 +1000

    backlog

---
+++ HackerRank/30_days_of_code/Day_1-Data_Types.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+d = 4.0
+# Read and save an integer, double, and String to your variables.
+s = "HackerRank "
+# Print the sum of both integer variables on a new line.
+user_int = int(input())
+user_double = float(input())
+user_string = input()
+# Print the sum of the double variables on a new line.
+print(i + user_int)
+print(f"{d + user_double:.1f}")
+print(s + user_string)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
